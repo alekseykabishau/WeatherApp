67994119a1c0e701cdfd6f4f1de0ac7c50a53c27
AlekseyK <aleksey.kabishau@gmail.com>
2020-06-26 23:32:08 -0400

    refactor: replace color and font literals

---
+++ WeatherApp/Configuration/Styles.swift
@@ -0,0 +1,24 @@
+//
+//  Styles.swift
+//  WeatherApp
+//
+//  Created by Aleksey on 0626..20.
+//  Copyright © 2020 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+
+enum Style {
+	
+	enum Colors {
+		static let base: UIColor = UIColor(red: 0.31, green: 0.72, blue: 0.83, alpha: 1.0)
+	}
+	
+	
+	enum Fonts {
+		static let lightRegular: UIFont = .systemFont(ofSize: 17.0, weight: .light)
+		static let lightSmall: UIFont = .systemFont(ofSize: 15.0, weight: .light)
+		static let heavyLarge: UIFont = .systemFont(ofSize: 20.0, weight: .heavy)
+	}
+}
--- WeatherApp/View Controllers/Weather VCs/DayVC.swift
+++ WeatherApp/View Controllers/Weather VCs/DayVC.swift
@@ -13,15 +13,15 @@ final class DayVC: UIViewController {
 	
 	@IBOutlet var dateLabel: UILabel! {
 		didSet {
-			dateLabel.textColor = UIColor(red: 0.31, green: 0.72, blue: 0.83, alpha: 1.0)
-			dateLabel.font = .systemFont(ofSize: 20.0, weight: .heavy)
+			dateLabel.textColor = Style.Colors.base
+			dateLabel.font = Style.Fonts.heavyLarge
 		}
 	}
 	
 	@IBOutlet var timeLabel: UILabel! {
 		didSet {
 			timeLabel.textColor = .black
-			timeLabel.font = .systemFont(ofSize: 15.0, weight: .light)
+			timeLabel.font = Style.Fonts.lightSmall
 		}
 	}
 	
@@ -34,21 +34,21 @@ final class DayVC: UIViewController {
 	@IBOutlet var temperatureLabel: UILabel! {
 		didSet {
 			temperatureLabel.textColor = .black
-			temperatureLabel.font = .systemFont(ofSize: 17.0, weight: .light)
+			temperatureLabel.font = Style.Fonts.lightRegular
 		}
 	}
 	
 	@IBOutlet var windSpeedLabel: UILabel! {
 		didSet {
 			windSpeedLabel.textColor = .black
-			windSpeedLabel.font = .systemFont(ofSize: 17.0, weight: .light)
+			windSpeedLabel.font = Style.Fonts.lightRegular
 		}
 	}
 	
 	@IBOutlet var descriptionLabel: UILabel! {
 		didSet {
 			descriptionLabel.textColor = .black
-			descriptionLabel.font = .systemFont(ofSize: 15.0, weight: .light)
+			descriptionLabel.font = Style.Fonts.lightSmall
 		}
 	}
 	
